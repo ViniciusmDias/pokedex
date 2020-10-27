64971714abaf34fe9f71a7a7e8e104c0b530fddb
Vinicius Dias <vinicius-mdias@hotmail.com>
2020-10-27 19:08:12 -0300

    Add Redux to project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import Routes from './routes';
 import GlobalStyle from './styles/global';
+import store from './store/store';
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <Routes />
       <GlobalStyle />
-    </>
+    </Provider>
   );
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import api from 'axios';
+import React from 'react';
 
-const URL = 'https://pokeapi.co/api/v2/pokemon?limit=20';
+import PokemonList from '../../containers/PokemonList';
 
 const Home = () => {
-  const [pokemons, setPokemons] = useState([]);
-  useEffect(() => {
-    api.get(URL).then((response) => {
-      setPokemons(response.data.results);
-
-      console.log(response.data.results);
-    });
-  }, []);
-
-  return <div />;
+  return (
+    <>
+      <PokemonList />
+    </>
+  );
 };
 export default Home;
