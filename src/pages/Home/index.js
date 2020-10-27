06da11b1621aab90a5ac51c2d65534a2f6cc71ab
Vinicius Dias <vinicius-mdias@hotmail.com>
2020-10-27 17:44:46 -0300

    Create global styles

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from 'axios';
+
+const URL = 'https://pokeapi.co/api/v2/pokemon?limit=20';
 
 const Home = () => {
-  return <h1>Home</h1>;
+  const [pokemons, setPokemons] = useState([]);
+  useEffect(() => {
+    api.get(URL).then((response) => {
+      setPokemons(response.data.results);
+
+      console.log(response.data.results);
+    });
+  }, []);
+
+  return <div />;
 };
 export default Home;
--- src/services/api.js
+++
@@ -1,7 +0,0 @@
-const URL = 'https://pokeapi.co/api/v2/pokemon?limit=20';
-
-const api = {
-  getAll: () => fetch(URL).then((response) => response.json()),
-};
-
-export default api;
